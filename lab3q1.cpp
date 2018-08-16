54f9adc119696c646bd3dc0828a6bce1d45e2921
ashishsahu321 <42150275+ashishsahu321@users.noreply.github.com>
2018-08-16 20:47:38 +0530

    Add files via upload

---
+++ lab3q1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+float b;
+double c;
+bool e;
+cout<<"size of a="<<sizeof(a)<<"\n";
+cout<<"size of b="<<sizeof(b)<<"\n";
+cout<<"size of c="<<sizeof(c)<<"\n";
+cout<<"size of e="<<sizeof(e)<<"\n";
+return 0;
+}
