9371c45b222625ab7b6e2e256786093e53a35401
ashishsahu321 <42150275+ashishsahu321@users.noreply.github.com>
2018-08-16 20:48:46 +0530

    Add files via upload

---
+++ lab3q2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int c,d,e,f;
+float g;
+cout<<"c=5+9"<<"\n";
+cout<<"d=5+(-9)"<<"\n";
+cout<<"e=5*9"<<"\n";
+cout<<"f=9%5"<<"\n";
+cout<<"g=5/9"<<"\n";
+return 0;
+}
