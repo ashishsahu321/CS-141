c9c231e045b76b8754861ca37b31b2c4a9ecfc98
ashishsahu321 <42150275+ashishsahu321@users.noreply.github.com>
2018-08-22 13:15:33 +0530

    Add files via upload

---
+++ lab4q7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float side,area;
+cout<<"enter the length of side of eqilateral tringle"<<endl;
+cin>>side;
+area=1.732*4*side*side;
+cout<<"the area of equilateral tringle is"<<area<<endl;
+return 0;
+}
