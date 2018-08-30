05e6a4b8bb7a2a66dd6d6d4289ed2a8444d604d2
ashishsahu321 <42150275+ashishsahu321@users.noreply.github.com>
2018-08-30 20:25:07 +0530

    Add files via upload

---
+++ ashish5q5.cpp
@@ -0,0 +1,25 @@
+
+//open library
+#include<iostream>
+using namespace std;
+int main()
+{
+//define the variable
+int a;
+//display statment
+cout<<"enter the value of a"<<endl;
+//store value in a
+cin>>a;
+//if condition
+if(a%2==0){
+//if condition is true then display 
+cout<<"a is even"<<endl;
+}
+//condition
+else{
+//if first condition is false display
+cout<<"a is odd";
+}
+return 0;
+}
+
