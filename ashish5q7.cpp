71dca4a6139a81525c0aa6192e7c95a39b6a5cfa
ashishsahu321 <42150275+ashishsahu321@users.noreply.github.com>
2018-09-04 15:41:03 +0530

    Add files via upload

---
+++ ashish5q7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,i;
+cout<<"enter the value of n"<<endl;
+cin>>n;
+while(i<n){
+cout<<"the number are"<<i<<endl;
+i++;
+}
+return 0;
+}
+
---
+++ ashish5q9.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char a='a';
+while(a<='z'){
+cout<<"the number are"<<endl<<a<<endl;
+a++;
+}
+return 0;
+}
